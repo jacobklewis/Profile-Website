4b80ed6a4a5a0dac12161030dea3bd2db245fc42
Jacob Lewis <jlewis34@uic.edu>
2020-07-26 23:01:45 -0500

    Added layout and projects

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+    sizeSections();
+};
+window.onresize = function () {
+    sizeSections();
+};
+window.onscroll = function () {
+    var pos = window.scrollY;
+    if (pos > 50) {
+        document.getElementsByTagName("nav")[0].classList.add("small-nav");
+    }
+    else {
+        document.getElementsByTagName("nav")[0].classList.remove("small-nav");
+    }
+};
+function sizeSections() {
+    var sectionSize = 0.75;
+    var elements = document.getElementsByClassName("section");
+    for (var index = 0; index < elements.length; index++) {
+        var element = elements[index];
+        element.style.height = window.innerHeight * sectionSize + "px";
+    }
+}
--- scripts/main.ts
+++ scripts/main.ts
@@ -1,3 +1,23 @@
-window.onload(new function () {
-    
-});
+window.onload = function () {
+    sizeSections()
+}
+window.onresize = function () {
+    sizeSections()
+}
+window.onscroll = function() {
+    const pos = window.scrollY
+    if (pos > 50) {
+        document.getElementsByTagName("nav")[0].classList.add("small-nav")
+    } else {
+        document.getElementsByTagName("nav")[0].classList.remove("small-nav")
+    }
+}
+
+function sizeSections() {
+    const sectionSize = 0.75
+    let elements = document.getElementsByClassName("section")
+    for (let index = 0; index < elements.length; index++) {
+        const element = elements[index] as HTMLElement;
+        element.style.height = `${window.innerHeight * sectionSize}px`
+    }
+}
